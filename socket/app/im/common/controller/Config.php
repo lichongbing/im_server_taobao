52554534a0dbcf70bb7c3b3dd06fab7e57560ed8
lichongbing <44672565+lichongbing@users.noreply.github.com>
2021-01-28 22:16:49 +0900

    Update Config.php

--- socket/app/im/common/controller/Config.php
+++ socket/app/im/common/controller/Config.php
@@ -8,7 +8,8 @@ class Config
     
     /** 获取本机ip */
     public static function getLocalIP() {
-        return "111.229.246.92";
+        //return "111.229.246.92";
+        return "121.4.163.89";
         // $preg = "/\A((([0-9]?[0-9])|(1[0-9]{2})|(2[0-4][0-9])|(25[0-5]))\.){3}(([0-9]?[0-9])|(1[0-9]{2})|(2[0-4][0-9])|(25[0-5]))\Z/";
         // exec("ipconfig", $out, $stats);
         // if (!empty($out)) {
